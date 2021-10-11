44eaa8746d1c815ce3e971d02df87847e55d013d
Nick <nsand3@gmail.com>
2021-10-10 17:29:33 -0700

    added Map component for mapbox with basic styles

---
+++ sptfy-wt-frontend/src/Map/Map.js
@@ -0,0 +1,77 @@
+import React, { useRef, useState, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import {
+  clearMapLayer,
+  selectedCountryLayer,
+  worldviewFilter,
+} from "./MapStyles.js";
+import "./Map.css";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibnNhbmRlIiwiYSI6ImNrdWFudnphMTBpbmkybm8zOXUzYXlsZnMifQ.7d4a8ZfjVEARvZRA-spWNg";
+
+function MapDevTo() {
+  const [lng, setLng] = useState(-34.5034);
+  const [lat, setLat] = useState(16.0569);
+  const [zoom, setZoom] = useState(2.27);
+  const [selectedCountry, setSelectedCountry] = useState("FR");
+
+  const mapContainer = useRef("");
+  const map = useRef(null);
+
+  /** Map Initialization on component mount */
+  // adds clear layer to map
+  // updates lat, long, zoom when user moves
+  useEffect(() => {
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+    // adds clear map layer
+    map.current.on("load", function () {
+      // add a clear layer with country boundaries
+      map.current.addLayer(clearMapLayer, "country-label");
+      //
+      map.current.addLayer(selectedCountryLayer, "country-label");
+    });
+
+    // highlights clicked on countries
+    map.current.on("click", "country-boundaries", function (e) {
+      console.log(e.features[0]);
+
+      map.current.setLayoutProperty(
+        "selected-country",
+        "visibility",
+        "visible"
+      );
+      let country = e.features[0].properties.iso_3166_1;
+      console.log(country);
+      setSelectedCountry(country);
+
+      map.current.setFilter("selected-country", ["==", "iso_3166_1", country]);
+    });
+
+    // changes cursor when hovering over layer
+    map.current.on("mouseenter", "country-boundaries", (e) => {
+      map.current.getCanvas().style.cursor = "pointer";
+    });
+    // updates state (lat, long, zoom) as user scrolls around
+    map.current.on("move", function () {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+    // this comment removes error message in browser
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="App">
+      <div ref={mapContainer} className="map-container"></div>
+    </div>
+  );
+}
+
+export default MapDevTo;
---
+++ sptfy-wt-frontend/src/Map/MapStyles.js
@@ -0,0 +1,54 @@
+// Spotify green #1DB954
+// Black #191414
+
+const worldviewFilter =
+  // [
+  //     "any",
+  //     ["==", "all", ["get", "worldview"]],
+  //     ["in", "US", ["get", "worldview"]]
+  // ];
+
+  // This creates blank green map with no disputed area issues
+  [
+    "all",
+    ["==", ["get", "disputed"], "false"],
+    ["match", ["get", "worldview"], ["all", "US"], true, false],
+  ];
+
+const clearMapLayer = {
+  id: "country-boundaries",
+  source: {
+    type: "vector",
+    url: "mapbox://mapbox.country-boundaries-v1",
+    worldview: "USA",
+  },
+  filter: worldviewFilter,
+  "source-layer": "country_boundaries",
+  type: "fill",
+  paint: {
+    "fill-outline-color": "#168d40",
+    "fill-color": "#ffffff",
+    "fill-opacity": 0,
+  },
+};
+
+const selectedCountryLayer = {
+  id: "selected-country",
+  source: {
+    type: "vector",
+    url: "mapbox://mapbox.country-boundaries-v1",
+  },
+  filter: worldviewFilter,
+  "source-layer": "country_boundaries",
+  type: "fill",
+  paint: {
+    "fill-outline-color": "#168d40",
+    "fill-color": "#1DB954",
+    "fill-opacity": 1,
+  },
+  layout: {
+    visibility: "none",
+  },
+};
+
+export { clearMapLayer, selectedCountryLayer, worldviewFilter };
