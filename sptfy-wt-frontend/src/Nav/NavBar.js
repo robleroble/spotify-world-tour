b6135a930d9aa2940a50735a8e81489d0eff8c66
Nick <nsand3@gmail.com>
2021-11-27 11:37:05 -0800

    added navbar for logged-in/out users

--- sptfy-wt-frontend/src/Nav/NavBar.js
+++ sptfy-wt-frontend/src/Nav/NavBar.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, NavbarBrand, NavLink, Nav, NavItem } from "reactstrap";
+import UserContext from "../UserContext";
 
 import "./NavBar.css";
 
@@ -8,22 +9,39 @@ function NavBar() {
   const spotifyLogin = () => {
     window.open("http://localhost:3000/auth/spotify", "_self");
   };
+  const { user } = useContext(UserContext);
 
-  return (
-    <Navbar className="d-flex justify-content-between mw-100">
-      <NavbarBrand className="ms-4 navbar-text">Spotify World Tour</NavbarBrand>
-      <Nav>
-        <NavItem>
-          <NavLink onClick={spotifyLogin} className="navbar-text">
-            Login
-          </NavLink>
-        </NavItem>
+  function loggedInNav() {
+    return (
+      <>
         <NavItem>
           <NavLink className="navbar-text">
             <Link to="profile">Profile</Link>
           </NavLink>
         </NavItem>
-      </Nav>
+        <NavItem>
+          <NavLink className="navbar-text">Logout</NavLink>
+        </NavItem>
+      </>
+    );
+  }
+
+  function loggedOutNav() {
+    return (
+      <>
+        <NavItem>
+          <NavLink onClick={spotifyLogin} className="navbar-text">
+            Login
+          </NavLink>
+        </NavItem>
+      </>
+    );
+  }
+
+  return (
+    <Navbar className="d-flex justify-content-between mw-100">
+      <NavbarBrand className="ms-4 navbar-text">Spotify World Tour</NavbarBrand>
+      <Nav>{user ? loggedInNav() : loggedOutNav()}</Nav>
     </Navbar>
   );
 }
